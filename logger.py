72e427f36d45be7a5282b295584527b8e3b9a78e
a <68975020+freepein@users.noreply.github.com>
2020-10-26 08:06:25 +0800

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,32 @@
+import sys, discord
+from discord.ext import commands
+import mysql.connector
+from datetime import datetime
+
+sql = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="discord_logger"
+    )
+cursor = sql.cursor()
+print(sql)
+
+date = datetime.today()
+date = date.strftime("%Y-%m-%d")
+
+client = commands.Bot(command_prefix='')
+
+@client.event
+async def on_ready():
+    print('logging servers')
+
+@client.event
+async def on_message(message):
+    sql_msg = "INSERT INTO logged_messages (guild, time_stamp, discord_username, discord_id, discord_message) VALUES (%s, %s, %s, %s, %s)"
+    val = (f"{message.guild.name}", date, f"{message.author}", f"{message.author.id}", f"{message.content}")
+    cursor.execute(sql_msg, val)
+    sql.commit()
+    print("Logged Message :D")
+
+client.run("PUT YOUR TOKEN HERE", bot=False)
